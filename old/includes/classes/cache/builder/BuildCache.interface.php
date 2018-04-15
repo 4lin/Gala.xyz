cafcdbcf727aa210384407935204b3cbbb62e14c
Serban Alin <4lin@users.noreply.github.com>
2018-04-15 15:44:53 +0200

    Add files via upload

--- old/includes/classes/cache/builder/BuildCache.interface.php
+++ old/includes/classes/cache/builder/BuildCache.interface.php
@@ -15,17 +15,7 @@
  * @link https://github.com/jkroepke/2Moons
  */
 
-class BannedBuildCache implements BuildCache
+interface BuildCache
 {
-	function buildCache()
-	{
-		$Data	= Core::getDB()->query("SELECT userID, MAX(banTime) FROM ".BANNED." WHERE banTime > ".TIMESTAMP." GROUP BY userID;");
-		$Bans	= array();
-		while($Row = $Data->fetchObject())
-		{
-			$Bans[$Row->userID]	= $Row;
-		}
-
-		return $Bans;
-	}
+	function buildCache();
 }
